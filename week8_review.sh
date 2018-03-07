6b21675796f5b96f9bd815b325a9d6b9647e077a
Mann <mannd00@my.saintpaul.edu>
2018-03-06 18:33:07 -0600

    Midterm Review Script

--- week8_review.sh
+++ week8_review.sh
@@ -3,18 +3,21 @@
 # Data Backup script that creates a service to perform backups of /home directory 
 
 echo "Hello! Welcome to the Back Up Tool Script!"
-echo "We will check what user's folders are in /home and craete a service to perform backups."
+echo "We will check what user's folders are in /home and craete a service to perform backups. \n"
 
 ## Figure out who/what/when/where
 ls -lh /home
-cd ~/
+
 MY_HOME= $HOME
-echo "Is this your home? $MY_HOME"
+echo "Is this your home? $MY_HOME \n"
+
+echo "Enter home dir or dir you want backed up"
+read -p "$HOME" MY_HOME
 
 BACKUPDIR=/mnt/backups
 
-echo "Where would you like the backups to be put? (Defaults to /mnt/backups)"
-read BACKUPDIR
+echo "Where would you like the backups to be put? (Defaults to /mnt/backups) \n"
+read -p "$BACKUPDIR" BACKUPDIR
 
 BACKUPTIME=weekly
 echo "How often you want backup? (daily, weekly, or monthly)"
@@ -26,13 +29,14 @@ touch AABackup.service
 
 echo "[Unit]" >> AABackup.service
 echo "Description=Back Up Tool Service" >> AABackup.service
-echo "Requires=media.mount" >> AABackup.service
+echo "Requires=" >> AABackup.service
 echo "[Service]" >> AABackup.service
 echo "Type=simple" >> AABackup.service
 echo "ExecStart=/usr/bin/rsync -avzr $MY_HOME $BACKUPDIR" >> AABackup.service
+echo "[Install]" >> AABackup.service
 echo "WantedBy=multi-user.target" >> AABackup.service
 
-#cp AABackup.service /etc/systemd/system/
+sudo cp AABackup.service /etc/systemd/system/
 
 touch AABackup.timer
 
@@ -44,15 +48,19 @@ echo "Persistent=True" >> AABackup.timer
 echo "[Install]" >> AABackup.timer
 echo "WantedBy=timers.target" >> AABackup.timer
 
-#cp AABackup.timer /etc/systemd/system/
+sudo cp AABackup.timer /etc/systemd/system/
 
 echo "And run it!"
-#sudo systemctl start AABackup.service
-#sudo systemctl start AABackup.timer
-#sudo systemctl enable AABackup.timer
+sudo systemctl start AABackup.service
+sudo systemctl start AABackup.timer
+sudo systemctl enable AABackup.timer
 
 echo "Now we can check it's status"
-#sudo systemctl status AABackup.service
+sudo systemctl status AABackup.service
 
 echo "And check all current jobs"
-#sudo systemctl list-jobs
+sudo systemctl list-jobs
+
+# Cleanup
+
+rm AABackup.*
